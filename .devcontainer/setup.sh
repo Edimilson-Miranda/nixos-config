a4e2563272c963bd9a4709c61d969f6fff1e8726
crayonnova <kaungminkhant.dev@gmail.com>
2026-04-17 18:07:32 +0000

    fix: codespace prebuild fix

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -4,4 +4,4 @@ export NIX_CONFIG="experimental-features = nix-command flakes"
 if ! command -v home-manager >/dev/null 2>&1; then
   nix profile install github:nix-community/home-manager
 fi
-home-manager switch --flake ".#vscode@codespaces"
+home-manager switch --flake ".#vscode@codespaces -b hmbackup"
