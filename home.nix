d4960ad00886e141f34ee4c3b235034555017852
crayonnova <kaungminkhant.dev@gmail.com>
2026-04-19 23:09:46 +0700

    fix: fixed nixos mcp

--- home.nix
+++ home.nix
@@ -49,8 +49,10 @@
           "type" = "local";
           "enabled" = true;
           "command" = [
-            "uvx"
-            "mcp-nixos"
+            "nix"
+            "run"
+            "github:utensils/mcp-nixos"
+            "--"
           ];
         };
       };
