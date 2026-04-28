90da395d13d4e8cedb32b56b2718db453e4909d1
crayonnova <kaungminkhant.dev@gmail.com>
2026-04-28 07:33:10 +0700

    niri - updated to unstable with overlays

--- modules/wayland/default.nix
+++ modules/wayland/default.nix
@@ -1,9 +1,21 @@
-{ lib, config, inputs, ... }:
 {
-  imports = [ inputs.niri.homeModules.niri ./packages.nix ];
+  lib,
+  config,
+  inputs,
+  pkgs,
+  ...
+}:
+{
+  imports = [
+    inputs.niri.homeModules.niri
+    ./packages.nix
+  ];
 
   config = lib.mkIf config.myconfig.features.desktop {
+    nixpkgs.overlays = [ inputs.niri.overlays.niri ];
+
     programs.niri.enable = true;
+    programs.niri.package = pkgs.niri-unstable;
 
     home.file.".config/niri/config.kdl" = {
       source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/stow/niri/.config/niri/config.kdl";
