b483db7c9e3c81caff2e5269ede7356581b713e0
Edimilson-Miranda <edimilsonsmp.464@gmail.com>
2026-06-08 18:26:52 -0300

    Adionando tema ao sddm

--- configuration.nix
+++ configuration.nix
@@ -8,7 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      inputs.silentSDDM.nixosModules.defaulta
+      inputs.silentSDDM.nixosModules.default
     ];
 
 	nix.settings.experimental-features = [
